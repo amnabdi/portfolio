4fa4dc874aabd90d0dcc2d7ae4081a4c333cb27e
amnabdi <aminabdidev@gmail.com>
2026-01-06 09:58:25 +0330

    change loading

--- src/components/common/loading/Loading.jsx
+++ src/components/common/loading/Loading.jsx
@@ -1,11 +1,11 @@
-import { HashLoader } from "react-spinners";
+import { PacmanLoader } from "react-spinners";
 
 const Loading = () => {
   return (
     <div
       className={`flex justify-center items-center fixed bottom-0 bg-white h-[100vh] w-[100vw] mx-auto z-50 `}
     >
-      <HashLoader color="#A53DFF" speedMultiplier={2} size={80} />
+      <PacmanLoader color="#1e40af" speedMultiplier={2} size={80} />
     </div>
   );
 };
